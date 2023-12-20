bbbd6be73b308bff898519f9a11fa2a33987c575
erickrapha <erick.r@escolar.ifrn.edu.br>
2023-12-20 17:08:13 -0300

    telaEscura

---
+++ My project (1)/Assets/Scripts/CarregarCena.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarregarCena : MonoBehaviour
+{
+    public string cenaParaCarregar;
+    
+    void Start()
+    {
+        SceneManager.LoadScene(cenaParaCarregar);
+    }
+    void Update()
+    {
+        
+    }
+}
--- My project (1)/Assets/Scripts/Digitacao.cs
+++ My project (1)/Assets/Scripts/Digitacao.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,6 +8,7 @@ using UnityEngine;
 public class Digitacao : MonoBehaviour
 {
     public bool imprimindo;
+    public float tempoEntreLetras = 0.1f;
 
     private TextMeshProUGUI componentText;
     private AudioSource sound;
@@ -16,21 +18,38 @@ public class Digitacao : MonoBehaviour
     {
         TryGetComponent(out componentText);
         TryGetComponent(out sound);
+        mensagemOriginal = componentText.text;
+        componentText.text = "";
     }
     private void OnEnable()
     {
-        
+        ImprimirMensagem(mensagemOriginal);
     }
     private void OnDisable()
     {
-        
+        componentText.text = mensagemOriginal;
+        StopAllCoroutines();
     }
-    public void ImprimirMensagem(string msg)
+    public void ImprimirMensagem(string mensagem)
     {
-
+        if (gameObject.activeInHierarchy)
+        {
+            if(imprimindo) return;
+            imprimindo = true;
+            StartCoroutine(LetraPorLetra(mensagem));
+        }
     }
-    IEnumerator LetraPorLetra(string msg)
+    IEnumerator LetraPorLetra(string mensagem)
     {
-        yield return null;
+        string msg = "";
+        foreach (var letra in mensagem)
+        {
+            msg += letra;
+            componentText.text = msg;
+            sound.Play();
+            yield return new WaitForSeconds(tempoEntreLetras);
+        }
+        imprimindo = false;
+        StopAllCoroutines();
     }
 }
