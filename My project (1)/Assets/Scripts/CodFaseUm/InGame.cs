b870bb2ca886187442d77d6af6df3f064cd45973
erickrapha <erick.r@escolar.ifrn.edu.br>
2024-01-03 17:57:14 -0300

    pausar

--- My project (1)/Assets/Scripts/CodFaseUm/InGame.cs
+++ My project (1)/Assets/Scripts/CodFaseUm/InGame.cs
@@ -8,15 +8,22 @@ public class InGame : MonoBehaviour
     public Text textPontuacao;
     public BarraVida barraVida;
 
+    [SerializeField]
+    private TelaPause telaPause;
     private Player jogador;
 
     private void Start()
     {
+        this.telaPause.Desativar();
         this.jogador = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
     }
     void Update()
     {
         this.barraVida.ExibirVida(this.jogador.Vida);
         this.textPontuacao.text = (ControladorPontuacao.Pontuacao + "x");
+    }
+    public void Pausar()
+    {
+        this.telaPause.Ativar();
     }
 }
